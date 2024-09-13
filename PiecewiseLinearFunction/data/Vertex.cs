7cf9929cc407ecacf5e9c7f8f0234678fac7ffd3
aleole <aleoleorl@gmail.com>
2024-09-13 23:58:49 +0300

    Correction of code oriented project.

---
+++ PiecewiseLinearFunction/data/Vertex.cs
@@ -0,0 +1,18 @@
+﻿namespace PiecewiseLinearFunction.data
+{
+    [Serializable]
+    public class Vertex
+    {
+        public Vertex(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public Vertex()
+        {
+
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
